9928c31f384f069307a5de96affaad2c2b3fec5b
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-29 14:49:30 +0100

    creato html, css, js, importata img logo

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Aggiungere i tasks da fare
+// Creare il bottone aggiungi task
+// Aggiungere gli errori: stringa vuota e minimo di caratteri
+// Evento da tastiera "Enter"
+// Cancellare automaticamente il Task aggiunto dalla lable
+// Eliminare i Tasks fatti 
+// Se non ci sono Tasks: "messaggio"
+
+// BONUS possibili (qualche idea ma sperimentate che volete):
+// curare l’interfaccia e renderla più carina e/o funzionale;
+// gestire anche lo stato “done” del task;
+// far succedere qualcosa al completamento del caricamento dell’applicazione;
+// etc..
+
+
+
+
+const {createApp} = Vue;
+
+    createApp({
+        data() {
+            return {
+                imageLogo: './img/To-Do.jpg' ,
+                tasks: [
+                    { 
+                        text: 'Fare i compiti', done: false 
+                    }, 
+                    { 
+                        text: 'Fare la spesa', done: true 
+                    }, 
+                    { 
+                        text: 'Fare il bucato', done: false 
+                    }
+                ]
+            }
+        },
+   
+    }).mount('#myapp')
